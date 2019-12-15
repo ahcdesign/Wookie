3ca1b72a82ddabf24f196b14b4ed347baafdf901
Angela Chih <angela.chih@alertwatch.com>
2019-12-15 17:01:52 -0500

    update

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View, Platform, AsyncStorage} from 'react-native';
+
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
+import { MainScreen } from './Main';
+import { Login } from './Login';
+import { AddClasses } from './AddClasses';
+import { Settings } from './Settings';
+import { AddStudents } from './AddStudents';
+import { Attendance } from './Attendance';
+import { AttendanceReport } from './AttendanceReport';
+
+
 const AppNavigator = createStackNavigator(
   {
-    Home: Courses,
-    Preferences: SettingsScreen,
-    CoursePreferences: CourseSettingScreen,
-    Course: RosterList,
+    Login: Login,
+    Home: MainScreen,
+    AddClasses: AddClasses,
+    Settings: Settings,
+    AddStudents: AddStudents,
+    Attendance: Attendance,
+    AttendanceReport: AttendanceReport
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Login',
   }
 );
+
 const AppContainer = createAppContainer(AppNavigator);
 export default AppContainer;
